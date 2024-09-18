28c8b3629ad4d2d05ca91a663c482e4ae3035b3a
yum <yumzi114@gmail.com>
2024-09-18 20:11:09 +0900

    d

--- master_bin/src/main.rs
+++ master_bin/src/main.rs
@@ -1,9 +1,7 @@
 
 use nb::block;
-use serde::{Deserialize, Serialize};
 use serde_derive;
 use serde_hex;
-use defaults::Defaults;
 use serde_hex::{SerHex,StrictPfx};
 use tokio::{self, task};
 use mini_redis::{client, Result};
@@ -13,11 +11,13 @@ use futures::{stream::StreamExt, SinkExt};
 use futures_channel::mpsc;
 use tokio_serial::{SerialPort, SerialPortBuilderExt, StopBits};
 use tokio_util::codec::{Decoder, Encoder};
-use masterapi::{self, LineCodec};
+use masterapi::{self, LineCodec,Packet};
 use tracing::{info, trace, warn, error};
 use tracing_subscriber;
 use linux_embedded_hal::I2cdev;
 use ads1x1x::{channel, Ads1x1x, ChannelSelection, ComparatorLatching, ComparatorMode, ComparatorPolarity, ComparatorQueue, DataRate16Bit, DynamicOneShot, FullScaleRange, ModeChangeError, SlaveAddr};
+use serde_derive::{Serialize, Deserialize};
+
 extern crate embedded_hal;
 
 
@@ -31,6 +31,12 @@ async fn main() -> Result<()> {
     tracing::subscriber::set_global_default(
         tracing_subscriber::FmtSubscriber::new()
     ).expect("setting default subscriber failed");
+    //=============ADS1115 변환기 설정=============
+    let mut app_report = Packet::default();
+    app_report.command = 0x03;
+    app_report.save("Report");
+
+    let report = Arc::new(Mutex::new(app_report));
     //=============ADS1115 변환기 설정=============
     let dev = I2cdev::new("/dev/i2c-1").unwrap();
     let address = SlaveAddr::default();
@@ -47,32 +53,82 @@ async fn main() -> Result<()> {
     adc.set_comparator_latching ( ComparatorLatching::Latching ) .unwrap ( ) ; 
     //=============ADS1115 공유메모리=============
     let arc_adc = Arc::new(Mutex::new(adc));
+    //=============센서상태 공유=============
+    let senser1_state = Arc::new(Mutex::new(false));
+    let senser2_state = Arc::new(Mutex::new(false));
     //=============시리얼설정=============
     let mut port = tokio_serial::new(SERIAL_DEVICE, 115200).open_native_async().unwrap();
     #[cfg(unix)]
     port.set_stop_bits(StopBits::One).unwrap();
     let (mut writer, mut reader) = LineCodec.framed(port).split();
+    //=============시리얼 센더 스레드=============
+    let report_mem = report.clone();
+    task::spawn(async move{
+        trace!("Serial Port Sender Open Device : {:?}",SERIAL_DEVICE);
+        loop{
+            let mut packet = *report_mem.lock().unwrap();
+            if let Ok(_)=packet.add_checksum(){
+                if let Ok(_)=packet.is_checksum(){
+                    if let Ok(file_data)=confy::load("master_app", Some("Report")){
+                        if packet !=file_data{
+                            if let Ok(_)=writer.send(packet).await{
+                                info!("SEND [REP]: {:?}",packet);
+                            }
+                        }
+                    }
+                }
+            };
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
     //=============시리얼 리더 스레드=============
     task::spawn(async move{
         #[cfg(unix)]
             // port.set_stop_bits(StopBits::One).unwrap();
             // let mut reader =LineCodec.framed(port);
-        trace!("Serial Port Open Device : {:?}",SERIAL_DEVICE);
+        trace!("Serial Port Reader Open Device : {:?}",SERIAL_DEVICE);
         while let Some(line_result) = reader.next().await {
             if let Ok(line)=line_result{
                 info!("READ [REQ]: {:?}", line);
-                println!("{:?}",line)
+                // println!("{:?}",line)
             }
         }
     });
     //=============센서1 측정=============
     let arc_mem = arc_adc.clone();
+    let senser_mem = senser1_state.clone();
+    let report_mem = report.clone();
     task::spawn(async move{
-        #[cfg(unix)]
+        // #[cfg(unix)]
         loop{
+
             let senser1 = block!(arc_mem.lock().unwrap().read(ChannelSelection::SingleA1)).unwrap();
+            if senser1 != report_mem.lock().unwrap().pannel_up{
+                report_mem.lock().unwrap().pannel_up = senser1;
+                report_mem.lock().unwrap().save("Report");
+            }
             match senser1 {
                 -32768..250=>{
+                    let mut list = vec![];
+                    for i in 0..10{
+                        thread::sleep(Duration::from_millis(1));
+                        list.push(
+                            block!(arc_mem.lock().unwrap().read(ChannelSelection::SingleA1)).unwrap()
+                        );
+                    }
+                    let all_check = list.iter().all(|&x|x < 250);
+                    if all_check {
+                        if !*senser_mem.lock().unwrap(){
+                           *senser_mem.lock().unwrap()=true;
+                        }
+                        // info!("READ [SENSER1]: DOWN");
+                        list.clear();
+                        thread::sleep(Duration::from_millis(1000));
+                    }
+                    else {
+                        list.clear();
+                        continue;
+                    }
                     
                 },
                 _=>{
@@ -85,13 +141,17 @@ async fn main() -> Result<()> {
                     }
                     let all_check = list.iter().all(|&x|x > 250);
                     if all_check {
-                        info!("READ [SENSER1]: DOWN");
+                        if *senser_mem.lock().unwrap(){
+                           *senser_mem.lock().unwrap()=false;
+                        }
+                        // info!("READ [SENSER1]: DOWN");
+                        list.clear();
                         thread::sleep(Duration::from_millis(1000));
                     }
                     else {
+                        list.clear();
                         continue;
                     }
-                    
                 }
             }
             thread::sleep(Duration::from_millis(1));
@@ -99,23 +159,87 @@ async fn main() -> Result<()> {
     });
     //=============센서2 측정=============
     let arc_mem = arc_adc.clone();
+    let senser_mem = senser2_state.clone();
+    let report_mem = report.clone();
     task::spawn(async move{
-        #[cfg(unix)]
+        // #[cfg(unix)]
         loop{
             let senser2 = block!(arc_mem.lock().unwrap().read(ChannelSelection::SingleA2)).unwrap();
+            if senser2 != report_mem.lock().unwrap().pannel_down{
+                report_mem.lock().unwrap().pannel_down = senser2;
+                report_mem.lock().unwrap().save("Report");
+            }
             match senser2 {
                 -32768..250=>{
+                    let mut list = vec![];
+                    for i in 0..10{
+                        thread::sleep(Duration::from_millis(1));
+                        list.push(
+                            block!(arc_mem.lock().unwrap().read(ChannelSelection::SingleA2)).unwrap()
+                        );
+                    }
+                    let all_check = list.iter().all(|&x|x < 250);
+                    if all_check {
+                        if !*senser_mem.lock().unwrap(){
+                           *senser_mem.lock().unwrap()=true;
+                        }
+                        // info!("READ [SENSER2]: DOWN");
+                        list.clear();
+                        thread::sleep(Duration::from_millis(1000));
+                    }
+                    else {
+                        list.clear();
+                        continue;
+                    }
                     
                 },
                 _=>{
-                    info!("READ [SENSER2]: {:?}", senser2);
+                    let mut list = vec![];
+                    for i in 0..10{
+                        thread::sleep(Duration::from_millis(1));
+                        list.push(
+                            block!(arc_mem.lock().unwrap().read(ChannelSelection::SingleA2)).unwrap()
+                        );
+                    }
+                    let all_check = list.iter().all(|&x|x > 250);
+                    if all_check {
+                        if *senser_mem.lock().unwrap(){
+                           *senser_mem.lock().unwrap()=false;
+                        }
+                        // info!("READ [SENSER2]: DOWN");
+                        list.clear();
+                        thread::sleep(Duration::from_millis(1000));
+                    }
+                    else {
+                        list.clear();
+                        continue;
+                    }
                 }
             }
             thread::sleep(Duration::from_millis(1));
         }
     });
-    
-    loop{
+    //=============모터부하 센서 측정=============
+    let arc_mem = arc_adc.clone();
+    let report_mem = report.clone();
+    task::spawn(async move{
+        loop{
+            let sensor = block!(arc_mem.lock().unwrap().read(ChannelSelection::SingleA0)).unwrap();
+            if report_mem.lock().unwrap().overload!=sensor{
+                report_mem.lock().unwrap().overload = sensor;
+                report_mem.lock().unwrap().save("Report");
+            }
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    let arc_mem = arc_adc.clone();
+    loop
+    {
+        let test = block!(arc_mem.lock().unwrap().read(ChannelSelection::SingleA0)).unwrap();
+        info!("LEVEL SENSER : {:?}",test);
+
+        info!("SENSER_1 STATE : {:?}",senser1_state.lock().unwrap());
+        info!("SENSER_2 STATE : {:?}",senser2_state.lock().unwrap());
         thread::sleep(Duration::from_millis(50));
     }
     Ok(())
--- masterapi/src/lib.rs
+++ masterapi/src/lib.rs
@@ -2,11 +2,13 @@
 
 use core::str;
 use std::io::{self, Error, ErrorKind};
-
+use serde_derive::{Serialize, Deserialize};
 use bytes::{BufMut, BytesMut};
 use tokio_util::codec::{Decoder, Encoder};
-
-
+use defaults::Defaults;
+use serde_with::{self, serde_as};
+use core::result::Result;
+// use serde_hex::{SerHex,StrictPfx};
 #[cfg(unix)]
 // const SERIAL_DEVICE: &'static str = env!("SERIAL_DEVICE");
 const SERIAL_DEVICE: &'static str = "/dev/ttyAMA4";
@@ -49,15 +51,115 @@ impl Decoder for LineCodec {
     }
 }
 
-impl Encoder<Vec<u8>> for LineCodec {
+// impl Encoder<Vec<u8>> for LineCodec {
+//     type Error = io::Error;
+
+//     fn encode(&mut self, item: Vec<u8>, buf: &mut BytesMut) -> Result<(), Self::Error> {
+        
+//         for i in item{
+//             buf.put_u8(i);
+//         }
+        
+//         Ok(())
+//     }
+// }
+
+impl Encoder<Packet> for LineCodec {
     type Error = io::Error;
 
-    fn encode(&mut self, item: Vec<u8>, buf: &mut BytesMut) -> Result<(), Self::Error> {
-        
-        for i in item{
-            buf.put_u8(i);
-        }
+    fn encode(&mut self, item: Packet, buf: &mut BytesMut) -> Result<(), Self::Error> {
+        buf.put_u8(item.start);
+        buf.put_u8(item.length);
+        buf.put_u16(item.reserved);
+        buf.put_u8(item.command);
+        buf.put_i16(item.pannel_up);
+        buf.put_i16(item.pannel_down);
+        buf.put_i16(item.overload);
+        buf.put_u8(item.sensor_state);
+        buf.put_u8(item.checksum);
+        buf.put_u8(item.end);
+        // for i in item{
+        //     buf.put_u8(i);
+        // }
         
         Ok(())
+    }
+}
+//=============Report 구조체 추상화=============
+#[derive(Debug,PartialEq,Eq,Serialize,Deserialize,Defaults,Clone,Copy)]
+pub struct Packet {
+    #[def = "0xAF"]
+    start: u8,
+    #[def = "0x0B"]
+    length: u8,
+    #[def = "0x0000"]
+    reserved: u16,
+    #[def = "0x00"]
+    pub command: u8,
+    #[def = "0x0000"]
+    pub pannel_up: i16,
+    #[def = "0x0000"]
+    pub pannel_down: i16,
+    #[def = "0x0000"]
+    pub overload: i16,
+    #[def = "0x00"]
+    pub sensor_state: u8,
+    #[def = "0x00"]
+    pub checksum: u8,
+    #[def = "0xFC"]
+    end: u8,
+}
+impl Packet{
+    pub fn save (&self, file_name : &str){
+        confy::store("master_app", Some(file_name), self).unwrap();
+    }
+    pub fn load (&self, file_name : &str){
+
+    }
+    pub fn add_checksum (&mut self)->Result<(),String>{
+        let mut sumdata:u128=0;
+        self.reserved.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        self.command.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        self.pannel_up.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        self.pannel_down.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        self.overload.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        self.sensor_state.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        let hex_str = format!("{:#x}",sumdata);
+        let check_sum =hex::decode(&hex_str[hex_str.len()-2..]);
+        if let Ok(data)=check_sum{
+            self.checksum = data[0];
+            return Ok(());
+        }
+        else{
+            let hex_str = hex_str.trim_start_matches("0x");
+            let checksum=u8::from_str_radix(hex_str,16).unwrap();
+            self.checksum = checksum;
+            return Ok(());
+        }
+    }
+    pub fn is_checksum (&self)->Result<(),String>{
+        let mut sumdata:u128=0;
+        self.reserved.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        self.command.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        self.pannel_up.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        self.pannel_down.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        self.overload.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        self.sensor_state.to_be_bytes().map(|x|sumdata+=u128::from(x));
+        let hex_str = format!("{:#x}",sumdata);
+        let check_sum =hex::decode(&hex_str[hex_str.len()-2..]);
+        if let Ok(data)=check_sum{
+            if self.checksum!=data[0]{
+                return Err("Fail checksum Err".to_string());
+            }
+            return Ok(());
+        }
+        else{
+            let hex_str = hex_str.trim_start_matches("0x");
+            let checksum=u8::from_str_radix(hex_str,16).unwrap();
+            if self.checksum!=checksum{
+                return Err("Fail checksum Err".to_string());
+            }
+            return Ok(());
+        }
     }
 }
